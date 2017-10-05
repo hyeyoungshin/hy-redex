9365f9b2a89321a32ed522fec5d437f0bb4f90e1
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2017-10-05 17:18:27 -0400

    added some comments

--- project1/examples/by-value2.rkt
+++ project1/examples/by-value2.rkt
@@ -2,12 +2,24 @@
 
 (def (y Num) 2)
 
-(def (x (→ (→ Num Num) Num)) (λ (t Num) (λ (f Num) t)))
+;(def (x (→ (→ Num Num) Num)) (λ (t Num) (λ (f Num) t)))
 
 
 
+;; test 1 -----------------------------------
+;y
+;; expected '(2 : Num)
+;; returned '(2 :)
 
-y
 
-
+;; test 2 -----------------------------------
 ;((x 66) 4)
+;; expected '(66 : Num)
+;; returned '(66 :)
+
+
+;; test 3 -----------------------------------
+(λ (t Num) (λ (f Num) t))
+;; expected '((λ (t Num) (λ (f Num) t)) :(→ Num (→ Num Num)))
+;; returned '((λ (t Num) (λ (f Num) t)) :) when the second definition is not commented out
+
