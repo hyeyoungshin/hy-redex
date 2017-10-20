f8287d57e26061d60d4f878a31755bcb0288973a
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2017-10-20 18:04:18 -0400

    examples to check typing works

--- project1/examples/by-name-tlc.rkt
+++ project1/examples/by-name-tlc.rkt
@@ -1,17 +1,6 @@
 #lang project1/NPCF-tlc
 
-(def x : Num 1)
+(def y : Num 2)
+(def add2 : (→ Num Num) (λ y : Num (+ y 2)))
 
-;(def y : (List Num) (cons 1 (nil Num)))
-
-x
-
-
-#;
-(def fib : (→ Num Num) (λ  m : Num (if0 m
-                                       0
-                                       (if0 (- m 1)
-                                            1
-                                            (+ (fib (- m 1)) (fib (- m 2)))))))
-
-;x
+(add2 y)
--- project1/examples/by-value-tlc.rkt
+++ project1/examples/by-value-tlc.rkt
@@ -3,6 +3,4 @@
 (def y : Num 2)
 (def add2 : (→ Num Num) (λ y : Num (+ y 2)))
 
-;(def (x (→ (→ Num Num) Num)) (λ (t Num) (λ (f Num) t)))
-
 (add2 y)
